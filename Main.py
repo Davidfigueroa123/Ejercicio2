babae78674c06d1a4d2e4b4dc5d033350f909b46
Davidfigueroa123 <figueroacamargod@gmail.com>
2026-05-13 02:40:31 +0000

    feat: implementar ciclo principal

--- Main.py
+++ Main.py
@@ -1,7 +1,14 @@
 saldo = 1000
 
-print("===== MENÚ DEL CAJERO =====")
-print("1. Consultar saldo actual")
-print("2. Depositar dinero")
-print("3. Retirar dinero")
-print("4. Salir del programa")
+while True:
+    print("===== MENÚ DEL CAJERO =====")
+    print("1. Consultar saldo actual")
+    print("2. Depositar dinero")
+    print("3. Retirar dinero")
+    print("4. Salir del programa")
+
+    opc = input("\nElige una opcion (1-4): ")
+
+    if opc == "4":
+        print("Hasta luego!")
+        break
